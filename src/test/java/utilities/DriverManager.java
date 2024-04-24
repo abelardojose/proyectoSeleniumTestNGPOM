d462be61d9fc591d4a3fc8f59b67365f7010ff79
Jose Abelardo <josecapunay19@gmail.com>
2024-04-24 16:04:35 -0500

    Parametrización de Browser por Línea de Comando

--- src/test/java/utilities/DriverManager.java
+++ src/test/java/utilities/DriverManager.java
@@ -1,6 +1,9 @@
 package utilities;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverManager {
     private final boolean runServer = System.getenv("JOB_NAME") != null;
@@ -14,8 +17,23 @@ public class DriverManager {
     }
 
     private void buildLocalDriver() {
-        Logs.debug("Inicializar driver");
-        final var driver = new ChromeDriver();
+        var browserProperty = System.getProperty("browser");
+
+        if (browserProperty == null) {
+            Logs.debug("Defaul driver CHROME");
+            browserProperty = "CHROME";
+        }
+
+        final var browser = Browser.valueOf(browserProperty.toUpperCase());
+        Logs.debug("Init Driver: %s", browser);
+
+        final var driver = switch (browser) {
+            case CHROME -> new ChromeDriver();
+            case SAFARI -> new SafariDriver();
+            case EDGE -> new EdgeDriver();
+            case FIREFOX -> new FirefoxDriver();
+        };
+
         Logs.debug("Maximizar pantalla");
         driver.manage().window().maximize();
         Logs.debug("Borrar cookies");
@@ -31,5 +49,12 @@ public class DriverManager {
     public void killDriver() {
         Logs.debug("Finalizar driver");
         new WebDriverProvider().get().quit();
+    }
+
+    private enum Browser {
+        CHROME,
+        FIREFOX,
+        SAFARI,
+        EDGE
     }
 }
